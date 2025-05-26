beced063328327a4aa44a37b845d00c98a8f652d
wesleizin23 <164011096+wesleizin23@users.noreply.github.com>
2025-05-26 14:28:47 -0300

    2

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
+[
+{
+enunciado: "Pergunta 1",
+alternativas: ["Alternativa 1", "Alternativa 2"],
+},
+{
+enunciado: "Pergunta 2",
+alternativas: ["Alternativa 1", "Alternativa 2"],
+},
+];
